60c4c3013d2d302be41ccb77d9ed0092f76afa8e
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-03-27 11:14:03 +0700

    remove unused import

--- examples/sharding.py
+++ examples/sharding.py
@@ -1,4 +1,3 @@
-import logging
 from interactions.ext.autosharder import AutoShardedClient
 
 bot = AutoShardedClient(token="token_here", disable_sync=False, shards=69)  # Note: if you don't provide shards then it will adjust to the recommended amount for your bot.
