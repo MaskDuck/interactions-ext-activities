6bc986fbda2a53576104b3a977461f18948ed299
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-03-27 12:25:58 +0700

    lint

--- examples/sharding.py
+++ examples/sharding.py
@@ -1,10 +1,14 @@
 from interactions.ext.autosharder import AutoShardedClient
 
-bot = AutoShardedClient(token="token_here", disable_sync=False, shards=69)  # Note: if you don't provide shards then it will adjust to the recommended amount for your bot.
+bot = AutoShardedClient(token="token_here", disable_sync=False, shards=69)
+# Note: if you don't provide shards then it will adjust to the
+# recommended amount for your bot.
+
 
 @bot.event
 async def on_ready():
     print("bot is now online.")
+
 
 @bot.command(name="intent", description="h")
 async def intent(ctx):
